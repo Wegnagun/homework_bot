1e80b4fdb6b455dcff59ead150ab2d12b6c3111e
Александр Фокин <wegnagun@bk.ru>
2022-05-25 17:04:26 +0300

    start bot

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import logging
+import sys
+import os
+import requests
+from dotenv import load_dotenv
+import telegram
+from telegram import ReplyKeyboardMarkup, Bot
+from telegram.ext import CommandHandler, Updater
+import time
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHATID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,30 +26,47 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
 
 def send_message(bot, message):
-    ...
+    """ Отправка ботом сообщений в чат """
+    logger.info('отправка сообщения')
+    return bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
+    """ получаем ответ с API домашки """
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    return requests.get(ENDPOINT, headers=HEADERS, params=params).json()
 
 
 def check_response(response):
-
-    ...
+    try:
+        homework = response['homeworks']
+        return homework
+    except KeyError as ex:
+        print('dddd')
 
 
 def parse_status(homework):
-    homework_name = ...
+    """парсим данные с ответа яндекс.домашка"""
+    homework_name = homework['homework_name']
     homework_status = ...
 
     ...
 
-    verdict = ...
+    verdict = homework['status']
 
     ...
 
@@ -49,7 +74,13 @@ def parse_status(homework):
 
 
 def check_tokens():
-    ...
+    """ проверяем доступность переменных окружения """
+    if (PRACTICUM_TOKEN is not None
+            and TELEGRAM_TOKEN is not None
+            and TELEGRAM_CHAT_ID is not None):
+        return True
+    else:
+        return False
 
 
 def main():
@@ -60,24 +91,21 @@ def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
-    ...
-
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
-
+            parse_status(homework)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            send_message(bot, message)
             time.sleep(RETRY_TIME)
         else:
-            ...
+            pass
 
 
 if __name__ == '__main__':
-    main()
+    print(get_api_answer(1653481042 - 2592000)['homeworks'])
+    # main()
